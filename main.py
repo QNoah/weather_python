fec6ce5873126a57e4d9441e4aa18b173b668957
Quinten <quinten.nagel01@gmail.com>
2025-09-08 19:28:54 +0200

    Temperature will get shown

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import requests
-
-city_name = "Rotterdam"
-country_code = "NL"
-state_code = ""
 api_key = "5339b820ce54ed0c0b4fa971adf7214b"
-#
-api = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&limit=1&appid={api_key}"
+api = ""
 # 
 #
 # # 
 def request_temperature(lon, lat):
     r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={api_key}")    
-    print(r.text)
+    return r.json()
 
-def get_location_codes(d): # make a return for this function
+
+def get_location_codes(d):
     lon = d[0]['lon']
     lat = d[0]['lat']
     print("Succesfully received longitude and latitude")
     return lon, lat
-    # request_temperature(lon, lat)
+
 
 def fetch_api():
-    r = requests.get(api) # we've gotta put this in a function for a return.
+    r = requests.get(api)
     if r.status_code != 200:
         print("Something went wrong, try again later!")
         return
     if r.status_code == 200:
         return r.json()
 
+
+def enter_location() -> str:
+    city_name = input("Enter your city name: ")
+    country_code = input("Enter your country (Format: NL, USA, ENG, DE): ")
+    state_code = input("Enter your state code (leave empty if you are not sure!): ")
+    return f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&limit=1&appid={api_key}"
+
+
+api = enter_location()
 data = fetch_api()
 lon, lat = get_location_codes(data)
-temperature_information = request_temperature(lon, lat)
+information = request_temperature(lon, lat)
 
-# if "__name__" == "__main__":
-#     # Here we gotta put something for the menu that questions which temperature we want
+print(information['main']['temp'])
