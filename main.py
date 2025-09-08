c7dd722e9739cdca02aef7c851ec7288854ca3c5
Quinten <quinten.nagel01@gmail.com>
2025-09-08 19:03:39 +0200

    functions has been added, and the correct data for Rotterdam gets added.

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
 import requests
-import json
 
 city_name = "Rotterdam"
 country_code = "NL"
 state_code = ""
-lon = ""
-lat = ""
-api = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&limit=1&appid=5339b820ce54ed0c0b4fa971adf7214b"
-#this one ^ is important to receive the latitude and longitude. with that another request should be done to get the temperature.
+api_key = "5339b820ce54ed0c0b4fa971adf7214b"
 #
+api = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&limit=1&appid={api_key}"
 # 
-# 
-# https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API key}
-# this one is important to get temperature ^^  
 #
+# # 
+def request_temperature(lon, lat):
+    r = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={api_key}")    
+    print(r.text)
 
-def get_location_codes(d):
+def get_location_codes(d): # make a return for this function
     lon = d[0]['lon']
     lat = d[0]['lat']
     print("Succesfully received longitude and latitude")
-    # request_temperature()
+    return lon, lat
+    # request_temperature(lon, lat)
 
-r = requests.get(api)
-if r.status_code != 200:
-    print("Something went wrong, try again later!")
-if r.status_code == 200:
-    data = r.json()
-    get_location_codes(data)
+def fetch_api():
+    r = requests.get(api) # we've gotta put this in a function for a return.
+    if r.status_code != 200:
+        print("Something went wrong, try again later!")
+        return
+    if r.status_code == 200:
+        return r.json()
 
+data = fetch_api()
+lon, lat = get_location_codes(data)
+temperature_information = request_temperature(lon, lat)
 
-
-    # Ik was gebleven met het ophalen van latitude en longitude
+# if "__name__" == "__main__":
+#     # Here we gotta put something for the menu that questions which temperature we want
